b4f5aa8f68bafc0af50a5fa9ae9b091dd503c66f
Manjula Lal <manjula_lal@hotmail.com>
2024-01-26 16:26:16 +0000

    initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,30 @@
 
 // Vanilla JavaScript Function
 
+//function addTwoNumbers (a, b) {
+//code block
+//return a + b;
+//}
+//let sum = addTwoNumbers (3, 5);
+//console.log (sum);
+
+
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) =>
+{
+//code block
+return a + b;
+}
+let sum = addTwoNumbers (3, 5);
+console.log (sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers (a, b) => (a + b); is also valid
+//const addTwoNumbers (a, b) => a + b;
 
 // Implicit Returns
+const saySomething = message => console.log (message);
+saySomething ("hello there");
+const sayHello =() => console.log ('hello');
 
 // Returning Multiple Lines
